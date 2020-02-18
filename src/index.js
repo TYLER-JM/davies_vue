54f7b03a323cf5c9cbb3316a13d4bcab26b2c984
tyler martin <t.mar92@gmail.com>
2020-02-18 16:26:54 -0500

    adds quicksand font and file-loader package

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+import './style.css';
+
 function component() {
   const elem = document.createElement('div');
   elem.innerHTML = 'Hello Davies';
+  elem.classList.add('title');
 
   return elem
 }
