677b6aa36fb3f2c96f5b20d85fedd6f9f8d3c44b
tyler martin <t.mar92@gmail.com>
2020-02-18 20:48:04 -0500

    small change

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import './style.css';
-
+import speak from './MyComponent';
 function component() {
   const elem = document.createElement('div');
+  const btn = document.createElement('button');
+
+  btn.innerHTML = 'Click me for a message in the console!';
+  btn.onclick = speak;
+
   elem.innerHTML = 'Hello Davies';
+  // elem.setAttribute('id', 'app');
   elem.classList.add('title');
+  elem.appendChild(btn);
 
   return elem
 }
