10a05db62694d50160b1cdc9c73d89da34f32118
tyler martin <t.mar92@gmail.com>
2020-02-21 17:12:36 -0500

    adds novel prop and create variable to store info to be sent via axios

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -4,68 +4,6 @@ import Vue from 'vue';
 import './style.css';
 import axios from 'axios';
 
-// const hardData = [
-//   {
-//     id: '1',
-//     novel_id: '1',
-//     first_name: 'Solomon',
-//     last_name: 'BridgeTower',
-//   },
-//   {
-//     id: '2',
-//     novel_id: '1',
-//     first_name: 'Griselda',
-//     last_name: 'Webster',
-//   },
-//   {
-//     id: '3',
-//     novel_id: '1',
-//     first_name: 'Fredegonde',
-//     last_name: 'Webster',
-//   },
-//   {
-//     id: '4',
-//     novel_id: '2',
-//     first_name: 'Dunstan',
-//     last_name: 'Ramsey',
-//   },
-//   {
-//     id: '5',
-//     novel_id: '2',
-//     first_name: 'Mary',
-//     last_name: 'Dempster',
-//   },
-//   {
-//     id: '6',
-//     novel_id: '3',
-//     first_name: 'Simon',
-//     last_name: 'Darcourt',
-//   },
-//   {
-//     id: '7',
-//     novel_id: '3',
-//     first_name: 'Maria',
-//     last_name: 'Theotoky',
-//   },
-//   {
-//     id: '8',
-//     novel_id: '3',
-//     first_name: 'Arthur',
-//     last_name: 'Cornish',
-//   },
-//   {
-//     id: '9',
-//     novel_id: '4',
-//     first_name: 'Francis',
-//     last_name: 'Cornish',
-//   },
-//   {
-//     id: '10',
-//     novel_id: '5',
-//     first_name: 'Jonathan',
-//     last_name: 'Hullah',
-//   },
-// ];
 
 const app = new Vue({
   el: '#app',
@@ -75,6 +13,7 @@ const app = new Vue({
     characters: [],
     first_name: '',
     last_name: '',
+    novel: '',
   },
   mounted() {
     axios
@@ -89,9 +28,14 @@ const app = new Vue({
       );
     },
     addCharacter() {
+      const data = {
+        first_name: this.first_name,
+        last_name: this.last_name,
+        novel: this.novel,
+      };
       axios
-        .post('http://localhost/davies_characters/index.php/characters/add', { first_name: this.first_name, last_name: this.last_name })
-        .then((res) => console.log(res.data.first_name))
+        .post('http://localhost/davies_characters/index.php/characters/add', data)
+        .then((res) => console.log(res.data))
         .catch((err) => console.log('the error: ', err));
     },
   },
