ef1cbd7411993ff4cf73f5755ed4f7da743ee2f1
tyler martin <t.mar92@gmail.com>
2020-02-23 10:17:43 -0500

    uses custom component for input elements

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -4,9 +4,34 @@ import Vue from 'vue';
 import './style.css';
 import axios from 'axios';
 
-const app = new Vue({
+Vue.component('custom-input', {
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  props: ['title'],
+  methods: {
+    addCharacter() {
+      const data = {
+        first_name: this.first_name,
+        last_name: this.last_name,
+        novel: this.novel,
+        year_published: this.year_published,
+      };
+      axios
+        .post('http://localhost/davies_characters/index.php/characters/add', JSON.stringify(data))
+        .then((res) => console.log(res.data))
+        .catch((err) => console.log('the error: ', err));
+    },
+  },
+  template: '<input :placeholder="title">',
+});
+
+new Vue({
   el: '#app',
   data: {
+    inputs: [],
     message: 'Hello User!',
     display: [],
     characters: [],
@@ -27,17 +52,13 @@ const app = new Vue({
         (char) => regex.test(char.first_name) || regex.test(char.last_name),
       );
     },
-    addCharacter() {
-      const data = {
-        first_name: this.first_name,
-        last_name: this.last_name,
-        novel: this.novel,
-        year_published: this.year_published,
-      };
-      axios
-        .post('http://localhost/davies_characters/index.php/characters/add', JSON.stringify(data))
-        .then((res) => console.log(res.data))
-        .catch((err) => console.log('the error: ', err));
+    showForm() {
+      this.inputs = [
+        { title: 'first_name' },
+        { title: 'last_name' },
+        { title: 'novel' },
+        { title: 'year_published' },
+      ];
     },
   },
 });
