383ad4fe58cda19faef4d6e05e80701054b7cc89
tyler martin <t.mar92@gmail.com>
2020-02-24 16:07:44 -0500

    adds searchresults component, and includes in SearchForm

---
+++ src/components/SearchFormResult.vue
@@ -0,0 +1,14 @@
+<template>
+  <li>
+    {{ character.first_name }}
+    {{ character.last_name }}
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    character: Object,
+  }
+}
+</script>
--- src/components/TheSearchForm.vue
+++ src/components/TheSearchForm.vue
@@ -1,3 +1,45 @@
 <template>
-  <h3>YOUR SEARCHING</h3>
+<div>
+  <h3>search for a character</h3>
+  <input v-model="searchTerm">
+  <input type="button" v-on:click="searchCharacters" value="Search!">
+  <ol>
+    <SearchFormResult
+      v-for="character in characters"
+      :key="character.first_name"
+      :character="character"
+    />
+  </ol>
+</div>
 </template>
+
+<script>
+import SearchFormResult from './SearchFormResult.vue';
+import axios from 'axios';
+
+export default {
+  components: {
+    SearchFormResult,
+  },
+  data() {
+    return {
+      searchTerm: '',
+      characters: [],
+      allData: [],
+    }
+  },
+  mounted() {
+    axios
+      .get('http://localhost/davies_characters/')
+      .then((res) => this.allData = res.data);
+  },
+  methods: {
+    searchCharacters() {
+      const regex = RegExp(this.searchTerm, 'gi');
+        this.characters = this.allData.filter(
+          (char) => regex.test(char.first_name) || regex.test(char.last_name),
+        );
+    }
+  }
+}
+</script>
