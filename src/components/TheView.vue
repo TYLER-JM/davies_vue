8d38ebf909da1eeb85c83e438e0254131f242a71
tyler martin <t.mar92@gmail.com>
2020-02-26 17:13:19 -0500

    makes axios call and adds data object

--- src/components/TheView.vue
+++ src/components/TheView.vue
@@ -1,3 +1,22 @@
 <template>
   <h2>You're viewing someone</h2>
 </template>
+
+<script>
+import axios from 'axios';
+export default {
+  props: {
+    selected: String
+  },
+  data() {
+    return {
+      stats: {}
+    }
+  },
+  mounted() {
+    axios
+      .get(`http://localhost/davies_characters/index.php/characters/${this.selected}`)
+      .then(res => this.stats = res.data);
+  }
+}
+</script>
